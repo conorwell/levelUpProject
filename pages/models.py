8a86eb4557c21619b58a248669092b73e5338576
conorwell <c_wellman@coloradocollege.edu>
2024-01-18 13:14:37 -0600

    website now displays summarized text below

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
+
+class Summary(models.Model):
+    title = models.CharField
+    text = models.CharField
+    summary = models.CharField
